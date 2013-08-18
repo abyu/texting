a7fb2c99671d174339fc0b99af154b88a7cf356b
Kishore <abyu.kishore@thoughtworks.com>
2013-08-18 12:12:55 +0530

    TextMessage object can be created from a cursor pbject.

--- src/main/com/skk/texting/domain/Person.java
+++ src/main/com/skk/texting/domain/Person.java
@@ -1,4 +1,59 @@
 package com.skk.texting.domain;
 
 public class Person {
+    private String name;
+    private String address;
+
+    public Person(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        if (address != null ? !address.equals(person.address) : person.address != null) return false;
+        if (name != null ? !name.equals(person.name) : person.name != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (address != null ? address.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+    public static Person fromAddress(String address) {
+        return null;  //To change body of created methods use File | Settings | File Templates.
+    }
 }
---
+++ src/main/com/skk/texting/factory/PersonFactory.java
@@ -0,0 +1,9 @@
+package com.skk.texting.factory;
+
+import com.skk.texting.domain.Person;
+
+public class PersonFactory {
+    public Person fromAddress(String address) {
+        return null;
+    }
+}
