02ecc1975bdaeb9ebcd30be567ac5ca2b5fa218d
Kishore <abyu.kishore@thoughtworks.com>
2013-08-18 13:22:14 +0530

    Refactor - Move code into respective classes

--- src/main/com/skk/texting/TextMessageAdaptor.java
+++ src/main/com/skk/texting/TextMessageAdaptor.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
 import android.widget.TextView;
+import com.skk.texting.domain.TextMessage;
+import com.skk.texting.factory.PersonFactory;
 
 public class TextMessageAdaptor extends CursorAdapter {
 
@@ -18,16 +20,15 @@ public class TextMessageAdaptor extends CursorAdapter {
     public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
         LayoutInflater layoutInflater = LayoutInflater.from(context);
         View view = layoutInflater.inflate(R.layout.msg_row, null);
-
         return view;
     }
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
+        if(view == null) return;
         TextView contactName = (TextView)view.findViewById(R.id.contact_name);
-        String person = cursor.getString(cursor.getColumnIndex("person"));
-        String address = cursor.getString(cursor.getColumnIndex("address"));
-        String text = person == null? address: person;
-        contactName.setText(text);
+
+        TextMessage textMessage = TextMessage.fromCursor(cursor, new PersonFactory(context.getContentResolver()));
+        contactName.setText(textMessage.getDisplayName());
     }
 }
