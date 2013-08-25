c642a6d6b0bdccd48f1fe4c780f3ef4cb38d8a7b
Kishore <abyu.kishore@thoughtworks.com>
2013-08-25 10:56:17 +0530

    Skip all drafts when fetching for messages

--- src/main/com/skk/texting/constants/TextMessageConstants.java
+++ src/main/com/skk/texting/constants/TextMessageConstants.java
@@ -20,4 +20,16 @@ public class TextMessageConstants {
     public static final String ERROR_CODE = "error_code";
     public static final String SEEN = "seen";
 
+
+    public static class MessageType {
+
+        public static String ALL = "0";
+        public static String INBOX = "1";
+        public static String SENT = "2";
+        public static String DRAFT = "3";
+        public static String OUTBOX = "4";
+        public static String FAILED = "5"; // for failed outgoing messages
+        public static String QUEUED = "6";
+    }
+
 }
