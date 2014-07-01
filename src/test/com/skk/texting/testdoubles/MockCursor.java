af2fef6405bdac6025dc59dbfe3af91d92b3f215
Kishore <abyu.kishore@gmail.com>
2014-07-01 19:39:23 +0530

    :wrench: fixing build, removing faulty annotation

--- src/test/com/skk/texting/testdoubles/MockCursor.java
+++ src/test/com/skk/texting/testdoubles/MockCursor.java
@@ -190,7 +190,7 @@ public class MockCursor implements Cursor {
         //To change body of implemented methods use File | Settings | File Templates.
     }
 
-    @Override
+   
     public Uri getNotificationUri() {
         return null;
     }
