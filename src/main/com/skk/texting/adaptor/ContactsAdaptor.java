61dbf9837f176719a7f538ad0c6feefc23313726
Kishore <abyu.kishore@gmail.com>
2014-11-16 17:54:19 +0530

    :new: Search for contact using name or number

--- src/main/com/skk/texting/adaptor/ContactsAdaptor.java
+++ src/main/com/skk/texting/adaptor/ContactsAdaptor.java
@@ -1,26 +1,41 @@
 package com.skk.texting.adaptor;
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.database.Cursor;
+import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.FilterQueryProvider;
 import android.widget.TextView;
 import com.skk.texting.R;
 import com.skk.texting.domain.Person;
 
 public class ContactsAdaptor extends CursorAdapter {
-    public ContactsAdaptor(Context context, Cursor c) {
+    public ContactsAdaptor(final Context context, Cursor c) {
         super(context, c, false);
+        setFilterQueryProvider(new FilterQueryProvider() {
+            @Override
+            public Cursor runQuery(CharSequence charSequence) {
+                String selection = ContactsContract.CommonDataKinds.Phone.HAS_PHONE_NUMBER + " = " + 1 + " and " + ContactsContract.CommonDataKinds.Phone.IN_VISIBLE_GROUP + " = " + 1 + " and " + ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " LIKE '%" + charSequence + "%' or " + ContactsContract.CommonDataKinds.Phone.NUMBER + " LIKE '"+ charSequence + "%'";
+                ContentResolver contentResolver = context.getContentResolver();
+
+                Cursor query = contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, selection, null, null);
+
+                return query;
+            }
+        });
     }
 
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
-       LayoutInflater inflater = LayoutInflater.from(context);
+        LayoutInflater inflater = LayoutInflater.from(context);
 
-       return inflater.inflate(R.layout.contact, null);
+        return inflater.inflate(R.layout.contact, null);
     }
+
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
--- src/main/com/skk/texting/viewwrapper/AllContactsWrapper.java
+++ src/main/com/skk/texting/viewwrapper/AllContactsWrapper.java
@@ -1,14 +1,51 @@
 package com.skk.texting.viewwrapper;
 
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import com.skk.texting.R;
+import com.skk.texting.adaptor.ContactsAdaptor;
 
 public class AllContactsWrapper {
 
     private View allContacts;
+    private EditText contactSearch;
 
     public void initialize(View allContacts){
 
         this.allContacts = allContacts;
+
+        contactSearch = (EditText) allContacts.findViewById(R.id.search_contact);
+
+
+        setUpHandlers();
     }
+
+    private void setUpHandlers() {
+        final ListView contactsList = (ListView) allContacts.findViewById(R.id.contacts);
+        contactSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+                ContactsAdaptor adapter = (ContactsAdaptor) contactsList.getAdapter();
+
+                adapter.getFilter().filter(charSequence);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+    }
+
+
 }
